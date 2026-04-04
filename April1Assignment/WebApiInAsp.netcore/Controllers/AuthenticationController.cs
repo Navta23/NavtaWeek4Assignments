78bcba1a20d44775a85f2a55ead0867a9a1d28ce
Navta23 <guptanavta@gmail.com>
2026-04-04 11:49:45 +0530

    april1assignment

---
+++ April1Assignment/WebApiInAsp.netcore/Controllers/AuthenticationController.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApiInAsp.netcore.Models;
+
+namespace WebApiInAsp.netcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<IdentityUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registeruser, string role)
+        {
+            var userExist = await _userManager.FindByEmailAsync(registeruser.Email);
+            if(userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response
+                    {
+                        Status = "Error",
+                        Message = "User already exists!"
+                    });
+            }
+
+            //add user in the db
+            IdentityUser user = new()
+            {
+                Email = registeruser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registeruser.Username
+            };
+
+            if(await _roleManager.RoleExistsAsync(role))
+            {
+                var result = await _userManager.CreateAsync(user, registeruser.Password);
+                if (!result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response
+                    {
+                        Status = "Error",
+                        Message = "User Failed to Create!"
+                    });
+                }
+
+                //add role to the user
+                await _userManager.AddToRoleAsync(user, role);
+                return StatusCode(StatusCodes.Status200OK,
+                    new Response
+                    {
+                        Status = "Success",
+                        Message = $"User created Successfully!"
+                });
+            }
+
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                new Response 
+                { 
+                     Status = "Error", 
+                     Message = "This Role Doesnot Exist." 
+                });
+            }
+        }
+
+        // Generate Token
+        private JwtSecurityToken GetToken(List<Claim> authClaims)
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddYears(2), //valid for 2 yrs
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] Login login)
+        {
+            var user = await _userManager.FindByNameAsync(login.Username);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, login.Password))
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+                var userRoles = await _userManager.GetRolesAsync(user);
+                foreach (var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+
+                var jwtToken = GetToken(authClaims);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                    expiration = jwtToken.ValidTo
+                });
+                //returning the token...
+
+            }
+
+            return Unauthorized();
+
+
+        }
+
+
+    }
+}
